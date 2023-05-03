dbcf12a9df2ed0d93b6838d4b6d58fbba1a97a42
Lenovo <Lenovo@DESKTOP-VDJMSF4.mshome.net>
2023-05-03 11:53:16 +0530

    Framework Repository

---
+++ Hybrid_Framework/src/generic/Generic_Class.java
@@ -0,0 +1,35 @@
+package generic;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Generic_Class implements Framework_constant
+{
+	public WebDriver driver;
+	@BeforeMethod
+	public void openApp()
+	{
+		System.setProperty(chrome_key,chrome_value);
+		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.get(URL);
+		
+	}
+	@AfterMethod
+	public void closeApp(ITestResult res)
+	
+	{
+		if(ITestResult.FAILURE==res.getStatus())
+		{
+			Capture_Screenshot.getScreenshot(driver);
+		}
+		driver.close();
+		
+	}
+
+}
---
+++ Hybrid_Framework/src/testscript/Login_Script.java
@@ -0,0 +1,27 @@
+package testscript;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import generic.Fetch_Data;
+import generic.Generic_Class;
+import pom.Pom;
+
+public class Login_Script extends Generic_Class 
+{
+	@Test
+	public void test() 
+	{
+		String un = Fetch_Data.get_data("Sheet1", 0, 0);
+		String pwd = Fetch_Data.get_data("Sheet1", 0, 1);
+		Pom p = new Pom(driver);
+		p.username(un);
+		Assert.fail();
+		p.password(pwd);
+		p.login_btn();
+		
+		
+		
+	}
+
+}
